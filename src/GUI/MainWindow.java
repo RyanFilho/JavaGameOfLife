55f2125af9effffa1f6f03c9d131fb209a840fe9
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2016-04-05 16:19:09 -0300

    refatoração no codigo da GUI

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,21 +1,15 @@
 package GUI;
 
-import java.awt.GridLayout;
-
 import javax.swing.JFrame;
-
 
 public class MainWindow {
 	public JFrame frame;
-	private GridLayout layout;
 	public MainWindow()
 	{
 		frame = new JFrame("Game of Life");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 600);
+		frame.setSize(700, 700);
 		frame.setLocationRelativeTo(null);		
-		
-		
-//		frame.setResizable(false);
+
 	}
 }
--- src/GUI/Principal.java
+++ src/GUI/Principal.java
@@ -5,8 +5,6 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -20,84 +18,78 @@ import Core.Mundo;
 import Core.Posicao;
 
 public class Principal{		
+	public static final int tamanhoMundo = 20;
+	public static final int velocidade = 200;
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		MainWindow window = new MainWindow();	
-		JPanel principal = new JPanel();
-		window.frame.setContentPane(principal);
+		JPanel principal = new JPanel(new GridLayout(tamanhoMundo,tamanhoMundo));
+		
+		window.frame.setContentPane(principal);	
+		
+		Mundo mundo = inicializarMapa();		
+		preencherMapa(mundo);		
+        inicializarJanela(window, principal, mundo);              
+        jogar(principal, mundo);
+        
+        
+	}
 
-		GridLayout layout = new GridLayout(10,10);		
-		principal.setLayout(layout);
-		
-		ArrayList<Lugar> mapa = new ArrayList<Lugar>();		
-		for(int i = 0; i < 10; i++)
-			for(int j = 0; j < 10; j++)
-				mapa.add(new Lugar(i, j, new CelulaMorta()));
-		
-		Mundo mundo = new Mundo(mapa);
+	private static void preencherMapa(Mundo mundo) {
 		mundo.InserirCelulaViva(new Posicao(3,1));
 		mundo.InserirCelulaViva(new Posicao(3,2));
 		mundo.InserirCelulaViva(new Posicao(3,3));
+		mundo.InserirCelulaViva(new Posicao(2,3));
+		mundo.InserirCelulaViva(new Posicao(1,2));
 		
+		mundo.InserirCelulaViva(new Posicao(15,1));
+		mundo.InserirCelulaViva(new Posicao(15,2));
+		mundo.InserirCelulaViva(new Posicao(15,3));
+		mundo.InserirCelulaViva(new Posicao(17,2));
+		mundo.InserirCelulaViva(new Posicao(16,3));
 		
-        for(int i = 0; i < mundo.getMapa().size(); i++){
-        	JPanel panel = new JPanel();        	
-        	panel.setBackground(mundo.getMapa().get(i).getCelula().getCor());
-        	panel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-//        	panel.add
-        	principal.add(panel); 
-        }
-        window.frame.setVisible(true);
-              
-        Timer timer = new Timer(500, new ActionListener(){
+		mundo.InserirCelulaViva(new Posicao(5,16));
+		mundo.InserirCelulaViva(new Posicao(5,17));
+		mundo.InserirCelulaViva(new Posicao(5,18));
+		
+		mundo.InserirCelulaViva(new Posicao(10,16));
+		mundo.InserirCelulaViva(new Posicao(10,17));
+		mundo.InserirCelulaViva(new Posicao(10,18));
+	}
+
+	private static Mundo inicializarMapa() {
+		ArrayList<Lugar> mapa = new ArrayList<Lugar>();		
+		for(int i = 0; i < tamanhoMundo; i++)
+			for(int j = 0; j < tamanhoMundo; j++)
+				mapa.add(new Lugar(i, j, new CelulaMorta()));		
+		Mundo mundo = new Mundo(mapa);
+		return mundo;
+	}
+
+	private static void jogar(JPanel principal, Mundo mundo) {
+		
+		Timer timer = new Timer(velocidade, new ActionListener(){
             @Override
-            public void actionPerformed(ActionEvent e) {
-//            	principal.getGraphics().clearRect(0, 0, 600, 600);
-            	
+            public void actionPerformed(ActionEvent e) {            	
                 mundo.Turn();            
                 Component[] components = principal.getComponents();
                 ArrayList<Lugar> map = mundo.getMapa();
                 for(int i = 0; i < components.length; i++){
                 	components[i].setBackground(map.get(i).getCelula().getCor());
-                	System.out.println(i+". "+components[i].toString());
-                } 
-//                System.out.println("Mudei!");
-                
+                }                 
             }
         });
         timer.start();
 	}
 	
-	class Input implements MouseListener{
-
-		@Override
-		public void mouseClicked(MouseEvent e) {
-			
-		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
-
+	private static void inicializarJanela(MainWindow window, JPanel principal, Mundo mundo) {
+		
+		for(int i = 0; i < mundo.getMapa().size(); i++){
+        	JPanel panel = new JPanel();        	
+        	panel.setBackground(mundo.getMapa().get(i).getCelula().getCor());
+        	panel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        	principal.add(panel); 
+        }
+        window.frame.setVisible(true);
 	}
 }
